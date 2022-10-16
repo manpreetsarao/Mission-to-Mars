8fdef80edd5326d1028be7641f254589855571ef
manpreetsarao <111101038+manpreetsarao@users.noreply.github.com>
2022-10-16 08:48:19 -0700

    Adding file with error

--- app.py
+++ app.py
@@ -17,5 +17,5 @@ def scrape():
    mars_data = scraping.scrape_all()
    mars.update_one({}, {"$set":mars_data}, upsert=True)
    return redirect('/', code=302)
-   if __name__ == "__main__":
+if __name__ == "__main__":
    app.run()
